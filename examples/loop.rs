d73f61926c5e71c279df43de964778163921a93f
Jordan Danford <jordandanford@gmail.com>
2025-09-24 13:23:50 -0700

    Move loop test to `examples`

--- examples/loop.rs
+++ examples/loop.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), Error> {
     let mut vm = VM::new();
     let mut module = Module::new(&mut vm);
 
-    let input = r#""It works!""#;
+    let input = r#"(loop [n 1e6] (if ($eq n 0) 'done (recur ($sub n 1))))"#;
     let value = module.eval_str(input)?;
     println!("{value}");
     Ok(())
